6c858ad2e9800746e9f834b21be9ee0fea89f4ab
Rodrigo Silveira <rodrigocnovos@gmail.com>
2023-09-01 19:55:07 -0300

    ediçoes

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,5 +1,23 @@
-# Caminho completo da imagem que você deseja definir como papel de parede
-$imagePath = "G:\backup rodrigo\script\startmenu 1.9.a\wallpaper\w1.jpg"
+$osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
+
+if ($osInfo.Caption -like "*Windows 7*") {
+    # Caminho completo da imagem que você deseja definir como papel de parede
+    $imagePath = ".\finalizacao\wallpaper\w7.jpg"
+
+}
+elseif ($osInfo.Caption -like "*Windows 8*") {
+    # Caminho completo da imagem que você deseja definir como papel de parede
+    $imagePath = ".\finalizacao\wallpaper\w8.jpg"
+}
+elseif ($osInfo.Caption -like "*Windows 10*") {
+    # Caminho completo da imagem que você deseja definir como papel de parede
+    $imagePath = ".\finalizacao\wallpaper\w10.jpg"
+}
+elseif ($osInfo.Caption -like "*Windows 11*") {
+    # Caminho completo da imagem que você deseja definir como papel de parede
+    $imagePath = ".\finalizacao\wallpaper\w11.jpg"
+}
+
 
 # Caminho da pasta "Imagens" no perfil do usuário
 $targetFolderPath = [System.IO.Path]::Combine($env:USERPROFILE, "Pictures")
@@ -12,6 +30,7 @@ if (-Not (Test-Path $targetFolderPath)) {
 # Copia a imagem para a pasta "Imagens"
 $targetImagePath = [System.IO.Path]::Combine($targetFolderPath, (Get-Item $imagePath).Name)
 Copy-Item $imagePath -Destination $targetImagePath -Force
+
 
 # Define a nova imagem como papel de parede
 $SPI_SETDESKWALLPAPER = 0x0014
