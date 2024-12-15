60f2a41ee4031ce24c274d0912589530ae2bb241
RODRIGO <rodrigo@microfacilrn.com.br>
2024-12-15 18:28:50 -0300

    correção de conflitos no ender

--- ENDER.ps1
+++ ENDER.ps1
@@ -60,15 +60,8 @@ $form.Text = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::GetE
 $form.Size = New-Object Drawing.Size(460, 655)
 $form.StartPosition = "CenterScreen"
 
-<<<<<<< HEAD
-
 $checkboxes = @()
 
-
-=======
-$checkboxes = @()
-
->>>>>>> main
 # Adicionar um rótulo e caixa de texto para nome do técnico
 $label = New-Object Windows.Forms.Label
 $label.Text = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::GetEncoding("ISO-8859-1").GetBytes("Nome do técnico responsável"))
@@ -113,28 +106,6 @@ function CriarCheckBox {
         $form.Controls.Add($checkbox)
         return $checkbox
     }
-<<<<<<< HEAD
-    
-    # Adicionar checkboxes
-    $checkboxData = @(
-        @{Texto = "Checar se faltam drivers"; Nome = ".\rel_driver.ps1"; Tag = "-Wait"},
-        @{Texto = "Instalar office 2024 x64 PT-BR ativado"; Nome = ".\office.ps1"; Tag = ""},
-        @{Texto = "Instalar pacote do Ninite + Anydesk"; Nome = ".\programas.ps1"; Tag = ""},
-        @{Texto = "Abir compartilhamento de arquivos"; Nome = ".\share.ps1"; Tag = ""},
-        @{Texto = "Forçar atualizações do Windows Update e Loja"; Nome = ".\wupdate.ps1"; Tag = ""},
-        @{Texto = "Selecionar programas para bloqueio no Firewall"; Nome = ".\list_program_firewall.ps1"; Tag = "-Wait"},
-        @{Texto = "Bloquear as atualizações"; Nome = ".\block.ps1"; Tag = "-Wait"},
-        @{Texto = "Criar ponto de restauração"; Nome = ".\restorepoint.ps1"; Tag = ""},
-        @{Texto = "Ativador Windows 10/11"; Nome = ".\licenca.ps1"; Tag = "-Wait"},
-        @{Texto = "Gerar relatório de saúde de bateria"; Nome = ".\bat.ps1"; Tag = ""},
-        @{Texto = "Abrir sites de testes de Teclado, Câmera e Microfone"; Nome = ".\test.ps1"; Tag = ""},
-        @{Texto = "Script para correções diversas"; Nome = ".\correction.ps1"; Tag = "-Wait"},
-        @{Texto = "Padronização, papel de parede, ícones de contatos e menus"; Nome = ".\wallpaper.ps1"; Tag = ""},
-        @{Texto = "Limpeza de temporários, arquivos da instalação e rastros de uso"; Nome = ".\limpeza.ps1"; Tag = "-Wait"}
-        )
-        
-        $yPosition = 90
-=======
 
 # Adicionar checkboxes
 $checkboxData = @(
@@ -155,7 +126,6 @@ $checkboxData = @(
 )
 
 $yPosition = 90
->>>>>>> main
 foreach ($data in $checkboxData) {
     $checkboxes += CriarCheckBox $data.Texto $data.Nome $data.Tag $yPosition
     $yPosition += 30
@@ -164,27 +134,11 @@ foreach ($data in $checkboxData) {
 # Atualizar dinamicamente a posição de elementos abaixo dos checkboxes
 $progressBarY = $yPosition + 20
 
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 # Configurar barra de progresso
 $progressBar = New-Object Windows.Forms.ProgressBar
 $progressBar.Location = New-Object Drawing.Point(20, $progressBarY)
 $progressBar.Size = New-Object Drawing.Size(400, 20)
 $progressBar.Minimum = 0
-<<<<<<< HEAD
-$progressBar.Maximum = $checkboxes.Count
-$progressBar.Value = 0
-$form.Controls.Add($progressBar)
-
-
-# Função para atualizar a barra de progresso e status
-function AtualizarBarra {
-    param ($atual, $total)
-    $progressBar.Value = $atual
-    $status.Text = "Executando item $atual de $total..."
-=======
 $progressBar.Maximum = 100
 $progressBar.Value = 0
 $form.Controls.Add($progressBar)
@@ -195,7 +149,6 @@ function AtualizarBarra {
     $percentual = [math]::Round(($atual / $total) * 100)
     $progressBar.Value = $percentual
     $status.Text = "Executando item $atual de $total... ($percentual%)"
->>>>>>> main
 }
 
 $status = New-Object Windows.Forms.Label
@@ -203,17 +156,9 @@ $status.AutoSize = $true
 $status.Location = New-Object Drawing.Point(20, $($progressBarY - 20))
 $form.Controls.Add($status)
 
-<<<<<<< HEAD
-
 # Botões
 $buttonY = $progressBarY + 40
 
-
-=======
-# Botões
-$buttonY = $progressBarY + 40
-
->>>>>>> main
 # Botão OK
 $buttonOK = New-Object Windows.Forms.Button
 $buttonOK.Location = New-Object Drawing.Point(80, $buttonY)
@@ -221,29 +166,13 @@ $buttonOK.Size = New-Object Drawing.Size(80, 30)
 $buttonOK.Text = "OK"
 $buttonOK.Enabled = $false
 $buttonOK.Add_Click({
-<<<<<<< HEAD
-    #Exigir desativação do antivírus
-=======
     # Exigir desativação do antivírus
->>>>>>> main
     Start-Process "powershell.exe" -ArgumentList ".\defender.ps1" -Wait -NoNewWindow
     
     $tecnicoOS = $textBox.Text
     $total = ($checkboxes | Where-Object { $_.Checked }).Count
     $atual = 0
     
-<<<<<<< HEAD
-    function ExecuteSelectedScripts {
-        param ($scriptPath, $text, $tag)
-        if (Test-Path $scriptPath) {
-            $status.Text = "Executando: $text"
-            $process = Start-Process powershell.exe -ArgumentList "-File $scriptPath" -NoNewWindow -PassThru
-            if ($tag) { $process.WaitForExit() }
-        } else {
-            Write-Host "Script não encontrado: $scriptPath"
-        }
-    }
-=======
     # Função para execução dos scripts e atualizar a interface
 # Função para execução dos scripts e atualizar a interface
 function ExecuteSelectedScripts {
@@ -270,7 +199,6 @@ function ExecuteSelectedScripts {
         Write-Host "Script não encontrado: $scriptPath"
     }
 }
->>>>>>> main
     
     foreach ($checkbox in $checkboxes) {
         if ($checkbox.Checked) {
@@ -301,17 +229,7 @@ $buttonUpdate.Text = [System.Text.Encoding]::UTF8.GetString([System.Text.Encodin
 $buttonUpdate.AutoSize = $true
 $buttonUpdate.Add_Click({ 
     Start-Process powershell.exe -ArgumentList "-File update_script.ps1" -NoNewWindow -PassThru
-<<<<<<< HEAD
-    
-    # $form.Close()
-     })
-$form.Controls.Add($buttonUpdate)
-
-
-
-=======
 })
 $form.Controls.Add($buttonUpdate)
 
->>>>>>> main
 $form.ShowDialog()
